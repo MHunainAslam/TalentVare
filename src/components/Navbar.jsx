fee977ec2019357a17fa10ef6867283eb1320a65
Hunain Aslam <hunainaslam.ha@gmail.com>
2025-03-13 03:51:28 +0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,26 +5,26 @@ import Button from './HeigherOrderComponents/Button'
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-xl bg-white fixed-top">
-            <div className="container">
+            <div className="container d-flex  flex-nowrap">
                 <div className="d-flex justify-content-between">
                     <Link className="navbar-brand" to="#">
                         <img src="/assets/images/logo.png" alt="" className='profile' />
                     </Link>
                 </div>
-                    <div className="input-group w-auto nav-search d-xl-none">
-                        <span className="input-group-text" id="basic-addon1">
-                            <i className="bi bi-search"></i>
-                        </span>
-                        <input type="text" className="form-control" placeholder="Search" aria-label="search" aria-describedby="basic-addon1" />
+                <div className="input-group w-auto nav-search d-xl-none">
+                    <span className="input-group-text" id="basic-addon1">
+                        <i className="bi bi-search"></i>
+                    </span>
+                    <input type="text" className="form-control" placeholder="Search" aria-label="search" aria-describedby="basic-addon1" />
+                </div>
+                <div className='d-flex'>
+                    <button className="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className='d-xl-none'>
+                        <img src="/assets/images/Rectangle 1516.png" alt="" />
                     </div>
-                    <div className='d-flex'>
-                        <button className="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className='d-xl-none'>
-                            <img src="/assets/images/Rectangle 1516.png" alt="" />
-                        </div>
-                    </div>
+                </div>
                 <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                     <div className="offcanvas-header">
                         <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
